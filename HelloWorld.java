e276470b9399efe822f42dd6ce045fba58dbd094
BalajiSivarajRajan <balaji.srb@gmail.com>
2017-02-03 20:43:08 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World DevOps");
-      System.out.println("DevOps - Auto build");
+      System.out.println("testing hook DevOps - Auto build");
       
    }
 }
