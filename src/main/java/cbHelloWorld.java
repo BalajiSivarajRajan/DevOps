a69ef2c4ffa0a072b9ba5c7e85e8b2de65e70f64
BalajiSivarajRajan <balaji.srb@gmail.com>
2017-02-16 21:06:33 +0000

    Update cbHelloWorld.java

--- src/main/java/cbHelloWorld.java
+++ src/main/java/cbHelloWorld.java
@@ -1,6 +1,6 @@
 public class cbHelloWorld {
      public String sayHello() {
-        return "Hello world -CI/CD !";
+        return "Hello world -CI/CD  test !";
     }
     public static void main(String[] args) {
         cbHelloWorld helloWorldObject = new cbHelloWorld();
